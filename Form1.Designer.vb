fde54baad056c228844cd9f687e4c3eda87f1f0f
Ricardo Morais <rmorais91@gmail.com>
2018-06-12 09:21:56 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,367 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtNumeroUm = New System.Windows.Forms.TextBox()
+        Me.txtNumeroCinco = New System.Windows.Forms.TextBox()
+        Me.txtNumeroQuatro = New System.Windows.Forms.TextBox()
+        Me.txtNumeroTres = New System.Windows.Forms.TextBox()
+        Me.txtNumeroDois = New System.Windows.Forms.TextBox()
+        Me.txtEstrelaDois = New System.Windows.Forms.TextBox()
+        Me.txtEstrelaUm = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtOutputUm = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtOutputDois = New System.Windows.Forms.TextBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.txtOutputTres = New System.Windows.Forms.TextBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.txtOutputQuatro = New System.Windows.Forms.TextBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.txtOutputCinco = New System.Windows.Forms.TextBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.txtOutputStarUm = New System.Windows.Forms.TextBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.txtOutputStarDois = New System.Windows.Forms.TextBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtNumeroUm
+        '
+        Me.txtNumeroUm.Location = New System.Drawing.Point(12, 68)
+        Me.txtNumeroUm.Name = "txtNumeroUm"
+        Me.txtNumeroUm.Size = New System.Drawing.Size(76, 22)
+        Me.txtNumeroUm.TabIndex = 0
+        '
+        'txtNumeroCinco
+        '
+        Me.txtNumeroCinco.Location = New System.Drawing.Point(340, 68)
+        Me.txtNumeroCinco.Name = "txtNumeroCinco"
+        Me.txtNumeroCinco.Size = New System.Drawing.Size(76, 22)
+        Me.txtNumeroCinco.TabIndex = 1
+        '
+        'txtNumeroQuatro
+        '
+        Me.txtNumeroQuatro.Location = New System.Drawing.Point(258, 68)
+        Me.txtNumeroQuatro.Name = "txtNumeroQuatro"
+        Me.txtNumeroQuatro.Size = New System.Drawing.Size(76, 22)
+        Me.txtNumeroQuatro.TabIndex = 2
+        '
+        'txtNumeroTres
+        '
+        Me.txtNumeroTres.Location = New System.Drawing.Point(176, 68)
+        Me.txtNumeroTres.Name = "txtNumeroTres"
+        Me.txtNumeroTres.Size = New System.Drawing.Size(76, 22)
+        Me.txtNumeroTres.TabIndex = 3
+        '
+        'txtNumeroDois
+        '
+        Me.txtNumeroDois.Location = New System.Drawing.Point(94, 68)
+        Me.txtNumeroDois.Name = "txtNumeroDois"
+        Me.txtNumeroDois.Size = New System.Drawing.Size(76, 22)
+        Me.txtNumeroDois.TabIndex = 4
+        '
+        'txtEstrelaDois
+        '
+        Me.txtEstrelaDois.Location = New System.Drawing.Point(97, 141)
+        Me.txtEstrelaDois.Name = "txtEstrelaDois"
+        Me.txtEstrelaDois.Size = New System.Drawing.Size(76, 22)
+        Me.txtEstrelaDois.TabIndex = 5
+        '
+        'txtEstrelaUm
+        '
+        Me.txtEstrelaUm.Location = New System.Drawing.Point(15, 141)
+        Me.txtEstrelaUm.Name = "txtEstrelaUm"
+        Me.txtEstrelaUm.Size = New System.Drawing.Size(76, 22)
+        Me.txtEstrelaUm.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(7, 112)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(220, 25)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Introduza as estrelas:"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Yellow
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(15, 182)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(401, 39)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Ordenar Números"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(453, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(184, 25)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "Chave Ordenada:"
+        '
+        'txtOutputUm
+        '
+        Me.txtOutputUm.BackColor = System.Drawing.Color.Yellow
+        Me.txtOutputUm.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtOutputUm.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOutputUm.Location = New System.Drawing.Point(470, 84)
+        Me.txtOutputUm.Name = "txtOutputUm"
+        Me.txtOutputUm.ReadOnly = True
+        Me.txtOutputUm.Size = New System.Drawing.Size(30, 23)
+        Me.txtOutputUm.TabIndex = 17
+        Me.txtOutputUm.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(459, 68)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(67, 69)
+        Me.PictureBox1.TabIndex = 18
+        Me.PictureBox1.TabStop = False
+        '
+        'txtOutputDois
+        '
+        Me.txtOutputDois.BackColor = System.Drawing.Color.Yellow
+        Me.txtOutputDois.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtOutputDois.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOutputDois.Location = New System.Drawing.Point(543, 84)
+        Me.txtOutputDois.Name = "txtOutputDois"
+        Me.txtOutputDois.ReadOnly = True
+        Me.txtOutputDois.Size = New System.Drawing.Size(30, 23)
+        Me.txtOutputDois.TabIndex = 19
+        Me.txtOutputDois.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(532, 68)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(67, 69)
+        Me.PictureBox2.TabIndex = 20
+        Me.PictureBox2.TabStop = False
+        '
+        'txtOutputTres
+        '
+        Me.txtOutputTres.BackColor = System.Drawing.Color.Yellow
+        Me.txtOutputTres.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtOutputTres.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOutputTres.Location = New System.Drawing.Point(616, 84)
+        Me.txtOutputTres.Name = "txtOutputTres"
+        Me.txtOutputTres.ReadOnly = True
+        Me.txtOutputTres.Size = New System.Drawing.Size(30, 23)
+        Me.txtOutputTres.TabIndex = 21
+        Me.txtOutputTres.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(605, 68)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(67, 69)
+        Me.PictureBox3.TabIndex = 22
+        Me.PictureBox3.TabStop = False
+        '
+        'txtOutputQuatro
+        '
+        Me.txtOutputQuatro.BackColor = System.Drawing.Color.Yellow
+        Me.txtOutputQuatro.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtOutputQuatro.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOutputQuatro.Location = New System.Drawing.Point(689, 84)
+        Me.txtOutputQuatro.Name = "txtOutputQuatro"
+        Me.txtOutputQuatro.ReadOnly = True
+        Me.txtOutputQuatro.Size = New System.Drawing.Size(30, 23)
+        Me.txtOutputQuatro.TabIndex = 23
+        Me.txtOutputQuatro.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(678, 68)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(67, 69)
+        Me.PictureBox4.TabIndex = 24
+        Me.PictureBox4.TabStop = False
+        '
+        'txtOutputCinco
+        '
+        Me.txtOutputCinco.BackColor = System.Drawing.Color.Yellow
+        Me.txtOutputCinco.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtOutputCinco.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOutputCinco.Location = New System.Drawing.Point(762, 84)
+        Me.txtOutputCinco.Name = "txtOutputCinco"
+        Me.txtOutputCinco.ReadOnly = True
+        Me.txtOutputCinco.Size = New System.Drawing.Size(30, 23)
+        Me.txtOutputCinco.TabIndex = 25
+        Me.txtOutputCinco.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
+        Me.PictureBox5.Location = New System.Drawing.Point(751, 68)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(67, 69)
+        Me.PictureBox5.TabIndex = 26
+        Me.PictureBox5.TabStop = False
+        '
+        'txtOutputStarUm
+        '
+        Me.txtOutputStarUm.BackColor = System.Drawing.Color.Yellow
+        Me.txtOutputStarUm.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtOutputStarUm.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOutputStarUm.Location = New System.Drawing.Point(563, 167)
+        Me.txtOutputStarUm.Name = "txtOutputStarUm"
+        Me.txtOutputStarUm.ReadOnly = True
+        Me.txtOutputStarUm.Size = New System.Drawing.Size(27, 23)
+        Me.txtOutputStarUm.TabIndex = 27
+        Me.txtOutputStarUm.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = CType(resources.GetObject("PictureBox6.Image"), System.Drawing.Image)
+        Me.PictureBox6.Location = New System.Drawing.Point(539, 143)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(82, 78)
+        Me.PictureBox6.TabIndex = 28
+        Me.PictureBox6.TabStop = False
+        '
+        'txtOutputStarDois
+        '
+        Me.txtOutputStarDois.BackColor = System.Drawing.Color.Yellow
+        Me.txtOutputStarDois.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtOutputStarDois.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOutputStarDois.Location = New System.Drawing.Point(651, 167)
+        Me.txtOutputStarDois.Name = "txtOutputStarDois"
+        Me.txtOutputStarDois.ReadOnly = True
+        Me.txtOutputStarDois.Size = New System.Drawing.Size(29, 23)
+        Me.txtOutputStarDois.TabIndex = 29
+        Me.txtOutputStarDois.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Image = CType(resources.GetObject("PictureBox7.Image"), System.Drawing.Image)
+        Me.PictureBox7.Location = New System.Drawing.Point(627, 143)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(82, 78)
+        Me.PictureBox7.TabIndex = 30
+        Me.PictureBox7.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(7, 38)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(230, 25)
+        Me.Label3.TabIndex = 31
+        Me.Label3.Text = "Introduza os Números:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ClientSize = New System.Drawing.Size(852, 290)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtOutputStarDois)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.txtOutputStarUm)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.txtOutputCinco)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.txtOutputQuatro)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.txtOutputTres)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.txtOutputDois)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.txtOutputUm)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtEstrelaUm)
+        Me.Controls.Add(Me.txtEstrelaDois)
+        Me.Controls.Add(Me.txtNumeroDois)
+        Me.Controls.Add(Me.txtNumeroTres)
+        Me.Controls.Add(Me.txtNumeroQuatro)
+        Me.Controls.Add(Me.txtNumeroCinco)
+        Me.Controls.Add(Me.txtNumeroUm)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Ordena Chave Euromilhões"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNumeroUm As TextBox
+    Friend WithEvents txtNumeroCinco As TextBox
+    Friend WithEvents txtNumeroQuatro As TextBox
+    Friend WithEvents txtNumeroTres As TextBox
+    Friend WithEvents txtNumeroDois As TextBox
+    Friend WithEvents txtEstrelaDois As TextBox
+    Friend WithEvents txtEstrelaUm As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtOutputUm As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents txtOutputDois As TextBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents txtOutputTres As TextBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents txtOutputQuatro As TextBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents txtOutputCinco As TextBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents txtOutputStarUm As TextBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents txtOutputStarDois As TextBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,64 @@
+﻿Public Class Form1
+
+    ' Função que ordena as arrays
+    Public Function sortNumberAndStars(ByVal listNumbers() As Integer) As Integer()
+        ' Orderna a array
+        Array.Sort(listNumbers)
+        ' Devolve a array ordenanda
+        Return listNumbers
+    End Function
+
+    ' Procedimento que imprime os numeros da chave
+    Public Sub printNumbers(ByVal numbers() As Integer)
+        ' atribui numeros ordenados
+        txtOutputUm.Text = numbers(0)
+        txtOutputDois.Text = numbers(1)
+        txtOutputTres.Text = numbers(2)
+        txtOutputQuatro.Text = numbers(3)
+        txtOutputCinco.Text = numbers(4)
+    End Sub
+
+    ' Procedimento que imprime os numeros da chave
+    Public Sub printStars(ByVal stars() As Integer)
+        ' atribui numeros ordenados
+        txtOutputStarUm.Text = stars(0)
+        txtOutputStarDois.Text = stars(1)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ' Declaracao das arrays de numeros e  das estrelas
+        Dim numbers = New Integer() {10, 20, 5, 15, 3}
+        Dim stars = New Integer() {10, 1}
+
+
+        ' Obtem os numeros da Chave
+        numbers(0) = txtNumeroUm.Text
+        numbers(1) = txtNumeroDois.Text
+        numbers(2) = txtNumeroTres.Text
+        numbers(3) = txtNumeroQuatro.Text
+        numbers(4) = txtNumeroCinco.Text
+
+        ' Obtem as estrelas da chave
+        stars(0) = txtEstrelaUm.Text
+        stars(1) = txtEstrelaDois.Text
+
+        ' Invoca a funcao para ordenar a Array dos numeros 
+        numbers = sortNumberAndStars(numbers)
+
+        txtNumeroUm.Text = numbers(0)
+        txtNumeroDois.Text = numbers(1)
+
+        ' Imprime Números da chave nas respetivas caixas de texto
+        printNumbers(numbers)
+
+        ' Invoca a funcao para ordenar a Array das estrelas 
+        stars = sortNumberAndStars(stars)
+
+        ' Imprime estrelas da chave nas respetivas caixas de texto
+        printStars(stars)
+
+    End Sub
+
+
+End Class
